000a02b12d194b6d693996585edb6ba82a961d8b
Adilson Pacheco <adilson.pacheco93@gmail.com>
2019-03-05 09:00:15 -0500

    step.1 set up the server

--- studentServer.js
+++ studentServer.js
@@ -10,10 +10,52 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
+// Bodyparser to process uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
+// Cross origin request
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "X-Requested-With");
 next();
+});
+
+// Import the required database connectivity code and set up a database connection
+var fs = require('fs');
+var pg = require('pg');
+var configtext =
+""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value
+pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+var split = configarray[i].split(':');
+config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+
+// Add a simple app.get to test out the connection
+app.get('/postgistest', function (req,res) {
+	pool.connect(function(err,client,done) {
+		if(err){
+			console.log("not able to get connection "+ err);
+			2 of 4
+			res.status(400).send(err);
+		}
+		client.query('SELECT name FROM london_poi' ,function(err,result) {
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+		res.status(200).send(result.rows);
+		});
+	});
 });
 
 
